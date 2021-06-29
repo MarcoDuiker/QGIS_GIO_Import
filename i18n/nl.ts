c78b276e9e15b8653c75aa3a9192ee303997ed61
MarcoDuiker <md@md-kwadraat.nl>
2021-06-29 13:06:15 +0200

    updated translation

--- i18n/nl.ts
+++ i18n/nl.ts
@@ -3,59 +3,89 @@
 <context>
     <name>GIOimport</name>
     <message>
-        <location filename="../gio_import.py" line="185"/>
+        <location filename="../gio_import.py" line="212"/>
         <source>&amp;GIO import plugin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gio_import.py" line="172"/>
+        <location filename="../gio_import.py" line="199"/>
         <source>GIO import</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../gio_import.py" line="194"/>
         <source>Select GIO gml file(s)</source>
-        <translation>Selecteer GIO gml bestand(en)</translation>
+        <translation type="obsolete">Selecteer GIO gml bestand(en)</translation>
     </message>
     <message>
-        <location filename="../gio_import.py" line="235"/>
+        <location filename="../gio_import.py" line="264"/>
         <source>Select at least one file to import.</source>
         <translation>Selecteer tenminste een bestand om te importeren.</translation>
     </message>
     <message>
-        <location filename="../gio_import.py" line="242"/>
+        <location filename="../gio_import.py" line="272"/>
         <source>Start</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../gio_import.py" line="244"/>
         <source>Importing GIO gml files ...</source>
-        <translation>Importeren GIO gml bestanden ...</translation>
+        <translation type="obsolete">Importeren GIO gml bestanden ...</translation>
     </message>
     <message>
-        <location filename="../gio_import.py" line="256"/>
+        <location filename="../gio_import.py" line="306"/>
         <source>Analyzing file </source>
         <translation>Analyseren bestand </translation>
     </message>
     <message>
-        <location filename="../gio_import.py" line="270"/>
+        <location filename="../gio_import.py" line="327"/>
         <source> from </source>
         <translation> van </translation>
     </message>
     <message>
-        <location filename="../gio_import.py" line="270"/>
+        <location filename="../gio_import.py" line="327"/>
         <source>Adding file </source>
         <translation>Toevoegen bestand </translation>
     </message>
     <message>
-        <location filename="../gio_import.py" line="276"/>
+        <location filename="../gio_import.py" line="341"/>
         <source>Failed to import</source>
         <translation>Importeren mislukt</translation>
     </message>
     <message>
         <location filename="../gio_import.py" line="285"/>
         <source>Importing GIO gml files done!</source>
-        <translation>Klaar met het importeren van GIO gml bestanden!</translation>
+        <translation type="obsolete">Klaar met het importeren van GIO gml bestanden!</translation>
+    </message>
+    <message>
+        <location filename="../gio_import.py" line="221"/>
+        <source>Select GIO .gml and/ or .xml file(s)</source>
+        <translation>Selecteer GIO .gml en/ of .xml bestand(en)</translation>
+    </message>
+    <message>
+        <location filename="../gio_import.py" line="274"/>
+        <source>Importing GIO files ...</source>
+        <translation>Importeren GIO bestanden ...</translation>
+    </message>
+    <message>
+        <location filename="../gio_import.py" line="295"/>
+        <source>Could not find gml file name in</source>
+        <translation>gml bestandsnaam niet gevonden in</translation>
+    </message>
+    <message>
+        <location filename="../gio_import.py" line="359"/>
+        <source>Failed to write style file %s</source>
+        <translation>Schrijven stijlbestand %s mislukt</translation>
+    </message>
+    <message>
+        <location filename="../gio_import.py" line="370"/>
+        <source>Error loading style: </source>
+        <translation>Laden stijl mislukt: </translation>
+    </message>
+    <message>
+        <location filename="../gio_import.py" line="377"/>
+        <source>Importing GIO files done!</source>
+        <translation>Importeren GIO bestanden voltooid!</translation>
     </message>
 </context>
 <context>
@@ -68,7 +98,7 @@
     <message>
         <location filename="../gio_import_dialog_base.ui" line="20"/>
         <source>Select GIO-gml files to import</source>
-        <translation>Selecteer GIO-gml bestanden om te importeren</translation>
+        <translation type="obsolete">Selecteer GIO-gml bestanden om te importeren</translation>
     </message>
     <message>
         <location filename="../gio_import_dialog_base.ui" line="32"/>
@@ -79,6 +109,11 @@
         <location filename="../gio_import_dialog_base.ui" line="62"/>
         <source>Help</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gio_import_dialog_base.ui" line="20"/>
+        <source>Select GIO xml and/ or gml files to import</source>
+        <translation>Selecteer GIO xml en/ of gml bestanden om te importeren</translation>
     </message>
 </context>
 </TS>
